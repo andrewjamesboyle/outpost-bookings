1deeb34e314e51d2712f35fa2e1d95ba1e9dd6b4
Andrew James Boyle <109110419+andrewjamesboyle@users.noreply.github.com>
2023-07-24 09:28:55 -0700

    dummy emails send to andrewboylecodes

--- api/sendgrid.js
+++ api/sendgrid.js
@@ -1,5 +1,27 @@
+const sgMail = require("@sendgrid/mail");
+
 async function sendCustomEmailWithSendgrid(emailData) {
-  // send accesslink and booking data to sendgrid API
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+  const msg = {
+    to: emailData.to,
+    from: emailData.from,
+    subject: emailData.subject,
+    text: emailData.text,
+    html: emailData.html,
+  };
+
+  try {
+    const resp = await sgMail.send(msg);
+    console.log("response from SG", resp[0].statusCode);
+    console.log("Email sent");
+  } catch (error) {
+    console.error(error);
+
+    if (error.response) {
+      console.error(error.response.body);
+    }
+  }
 }
 
 module.exports = sendCustomEmailWithSendgrid;
--- api/webhook-listener.js
+++ api/webhook-listener.js
@@ -16,8 +16,18 @@ module.exports = async (req, res) => {
   const accessLink = await generateAccessLinkWithKisi(bookingData);
   console.log("accessLink", accessLink);
 
+  // Build object to pass required info to SendGrid
+  const emailData = {
+    to: bookingData.member_email,
+    from: "andrewboylecodes@gmail.com",
+    subject: "Booking Confirmation",
+    text: `Your booking has been confirmed. Use this access code to enter the building. ${accessLink}`,
+    html: `<p>Your booking has been confirmed. Use this access code to enter the building. ${accessLink}</p>`,
+  };
+
   // Send custom email with SendGrid API
-  //   await sendCustomEmailWithSendgrid(bookingData, accessLink);
+  await sendCustomEmailWithSendgrid(emailData);
+  console.log("emailData", emailData);
 
   // Send response back to Optix
   res.status(200).send("Handled the booking!");
