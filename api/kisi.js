504156195e6fe500b89476adea98c8252659c95d
Andrew James Boyle <109110419+andrewjamesboyle@users.noreply.github.com>
2023-07-26 13:52:13 -0700

    timezone updated to match UTC

--- api/kisi.js
+++ api/kisi.js
@@ -3,14 +3,15 @@ const axios = require("axios");
 const moment = require("moment-timezone");
 
 async function generateAccessLinkWithKisi(bookingData) {
-  // Parse datetime string into a moment object, specify its time zone as PST.
-  let checkInDate = moment.tz(
-    bookingData.check_in_datetime,
+  // Parse the datetime string into a Date object.
+  let checkInDate = moment
+    .tz(bookingData.check_in_datetime, "America/Los_Angeles")
+    .subtract(2, "hours");
+
+  let checkOutDate = moment.tz(
+    bookingData.check_out_datetime,
     "America/Los_Angeles"
   );
-
-  // Subtract 2 hours from check in date.
-  checkInDate.subtract(2, "hours");
 
   try {
     const response = await axios.post(
@@ -20,7 +21,7 @@ async function generateAccessLinkWithKisi(bookingData) {
           name: bookingData.member_name,
           group_id: 148832,
           valid_from: checkInDate.toISOString(), // Convert the Date object back to a string.
-          valid_until: bookingData.check_out_datetime,
+          valid_until: checkOutDate.toISOString(),
         },
       },
       {
